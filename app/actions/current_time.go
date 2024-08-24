c5ece5b715e45dd5e4f6b7a3e3eb37dadcc69df0
Jesse Chavez <jesse.chavez.r@gmail.com>
2024-08-24 19:20:48 +1000

    Test database connection and finding record

--- app/actions/current_time.go
+++ app/actions/current_time.go
@@ -1,18 +1,30 @@
 package actions
 
 import (
+	"context"
 	"fmt"
+	"my_app/app/models"
 	"net/http"
-	"time"
+	"strings"
+
+	"github.com/go-rel/rel/where"
 )
 
 func (axn *Action) CurrentTime(w http.ResponseWriter, r *http.Request) {
-	now := time.Now()
-	res := now.String()
+	account := models.Account{}
 
-	delim := "========================================================="
+	ctx := context.Background()
 
-	output := fmt.Sprintf("%s\n%s\n%s\n", delim, res, delim)
+	axn.repo.Find(ctx, &account, where.Eq("id", 1))
 
-	w.Write([]byte(output))
+	var res strings.Builder
+
+	res.WriteString(fmt.Sprintln("================================================="))
+	res.WriteString(fmt.Sprintln("Created At:", account.CreatedAt))
+	res.WriteString(fmt.Sprintln("Updated At:", account.UpdatedAt))
+	res.WriteString(fmt.Sprintln("  Nickname:", account.Nickname))
+	res.WriteString(fmt.Sprintln("     Email:", account.Email))
+	res.WriteString(fmt.Sprintln("================================================="))
+
+	w.Write([]byte(res.String()))
 }
